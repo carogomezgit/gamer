4ef07922090857501e84773dd11ea55b2767ac0d
Ara <aragomezgd@gmail.com>
2026-06-29 19:27:31 -0300

    update record TorneoResponseDTO

--- src/main/java/edu/isp63/prog2/gamer/dto/TorneoResponseDTO.java
+++ src/main/java/edu/isp63/prog2/gamer/dto/TorneoResponseDTO.java
@@ -3,6 +3,7 @@ package edu.isp63.prog2.gamer.dto;
 import java.math.BigDecimal;
 
 public record TorneoResponseDTO(
+    Integer id,
     String nombreTorneo,
     String nombreJuego,
     BigDecimal precio,
